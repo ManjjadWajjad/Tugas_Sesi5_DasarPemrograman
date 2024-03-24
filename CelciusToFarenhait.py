7f31be2ad939e8f494092d18801a35641a4d84ab
ManjjadWajjad <muhammadfaisalmuzhaffar@gmail.com>
2024-03-24 10:04:52 +0700

    first commit

---
+++ CelciusToFarenhait.py
@@ -0,0 +1,7 @@
+nama = 'Muhammad Faisal Muzhaffar' 
+matkul = 'Dasar Pemprograman'
+print(f'Halo Selamat Datang {nama}, Selamat Belajar {matkul}')
+
+celcius = float(input("Masukan Suhu dalam celcius"))
+farenthait=(celcius* 9/5)+32
+print("suhu dalam farenthait:",farenthait)
